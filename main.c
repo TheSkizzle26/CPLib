0038e86570a92ef234de5ea733c91f8c58331aca
TheSkizzle26 <timobarth12@outlook.de>
2026-04-09 22:13:12 +0200

    improved main.c

--- main.c
+++ main.c
@@ -18,32 +18,41 @@ APP_AUTHOR("TheSkizzle")
 APP_VERSION("0.0.0")
 
 
+static bool is_running;
+
+
+void update() {
+    if (cpIsKeyPressed(KEY_CLEAR)) {
+        is_running = false;
+    }
+}
+
+void render() {
+    cpBeginDrawing();
+    cpClearBackground(BLACK);
+
+    for (int j = 0; j < 100; j++)
+        cpDrawPixel(j, j, RED);
+
+    cpDrawRectangle(
+        150,
+        250,
+        100,
+        50,
+        SKYBLUE
+    );
+
+    cpEndDrawing();
+}
+
 int main() {
     cpInit();
+    is_running = true;
 
-    // ReSharper disable once CppDFAEndlessLoop
-    while (true) {
-        // update
-        if (cpIsKeyPressed(KEY_CLEAR)) {
-            break;
-        }
-
-        // render
-        cpBeginDrawing();
-        cpClearBackground(BLACK);
-
-        for (int j = 0; j < 100; j++)
-            cpDrawPixel(j, j, RED);
-
-        cpDrawRectangle(
-            150,
-            250,
-            100,
-            50,
-            SKYBLUE
-        );
-
-        cpEndDrawing();
+    // ReSharper disable once CppDFALoopConditionNotUpdated
+    while (is_running) {
+        update();
+        render();
     }
 
     cpQuit();
