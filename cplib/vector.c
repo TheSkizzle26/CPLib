3178ee241a96a8081ca04848e7e9f53d5a937f3c
TheSkizzle26 <timobarth12@outlook.de>
2026-04-12 16:07:09 +0200

    replaced vector3 functions with macros, started adding vec2 stuff

--- cplib/vector.c
+++ cplib/vector.c
@@ -3,80 +3,83 @@
 
 // TODO: replace with macros since we're just repeating the
 // TODO: same things over and over again
+// TODO: maybe make more functions inline
+
+#define F2_OP(op) return (cpVector2) { \
+    fix16_##op(a.x, b.x), \
+    fix16_##op(a.y, b.y), \
+};
+#define I2_OP(op) return (cpVector2i) { \
+    (a.x) op (b.x), \
+    (a.y) op (b.y), \
+};
+#define F2_OP_VAL(op) return (cpVector2) { \
+    fix16_##op(a.x, b), \
+    fix16_##op(a.y, b), \
+};
+#define I2_OP_VAL(op) return (cpVector2i) { \
+    (a.x) op (b), \
+    (a.y) op (b), \
+};
+
+#define F3_OP(op) return (cpVector3) { \
+    fix16_##op(a.x, b.x), \
+    fix16_##op(a.y, b.y), \
+    fix16_##op(a.z, b.z), \
+};
+#define I3_OP(op) return (cpVector3i) { \
+    (a.x) op (b.x), \
+    (a.y) op (b.y), \
+    (a.z) op (b.z), \
+};
+#define F3_OP_VAL(op) return (cpVector3) { \
+    fix16_##op(a.x, b), \
+    fix16_##op(a.y, b), \
+    fix16_##op(a.z, b), \
+};
+#define I3_OP_VAL(op) return (cpVector3i) { \
+    (a.x) op (b), \
+    (a.y) op (b), \
+    (a.z) op (b), \
+};
 
 
 /* cpVector3 */
 
 cpVector3 cpVector3Add(const cpVector3 a, const cpVector3 b) {
-    return (cpVector3) {
-        fix16_add(a.x, b.x),
-        fix16_add(a.y, b.y),
-        fix16_add(a.z, b.z),
-    };
+    F3_OP(add)
 }
 
 cpVector3 cpVector3AddValue(const cpVector3 a, const fix16_t b) {
-    return (cpVector3) {
-        fix16_add(a.x, b),
-        fix16_add(a.y, b),
-        fix16_add(a.z, b),
-    };
+    F3_OP_VAL(add)
 }
 
 cpVector3 cpVector3Subtract(const cpVector3 a, const cpVector3 b) {
-    return (cpVector3) {
-        fix16_sub(a.x, b.x),
-        fix16_sub(a.y, b.y),
-        fix16_sub(a.z, b.z),
-    };
+    F3_OP(sub)
 }
 
 cpVector3 cpVector3SubtractValue(const cpVector3 a, const fix16_t b) {
-    return (cpVector3) {
-        fix16_sub(a.x, b),
-        fix16_sub(a.y, b),
-        fix16_sub(a.z, b),
-    };
+    F3_OP_VAL(sub)
 }
 
 cpVector3 cpVector3Multiply(const cpVector3 a, const cpVector3 b) {
-    return (cpVector3) {
-        fix16_mul(a.x, b.x),
-        fix16_mul(a.y, b.y),
-        fix16_mul(a.z, b.z),
-    };
+    F3_OP(mul)
 }
 
 cpVector3 cpVector3MultiplyValue(const cpVector3 a, const fix16_t b) {
-    return (cpVector3) {
-        fix16_mul(a.x, b),
-        fix16_mul(a.y, b),
-        fix16_mul(a.z, b),
-    };
+    F3_OP_VAL(mul)
 }
 
 cpVector3 cpVector3Divide(const cpVector3 a, const cpVector3 b) {
-    return (cpVector3) {
-        fix16_div(a.x, b.x),
-        fix16_div(a.y, b.y),
-        fix16_div(a.z, b.z),
-    };
+    F3_OP(div)
 }
 
 cpVector3 cpVector3DivideValue(const cpVector3 a, const fix16_t b) {
-    return (cpVector3) {
-        fix16_div(a.x, b),
-        fix16_div(a.y, b),
-        fix16_div(a.z, b),
-    };
+    F3_OP_VAL(div)
 }
 
-cpVector3 cpVector3Negate(const cpVector3 v) {
-    return (cpVector3) {
-        -v.x,
-        -v.y,
-        -v.z
-    };
+inline cpVector3 cpVector3Negate(const cpVector3 v) {
+    return cpVector3MultiplyValue(v, fix16_from_int(-1));
 }
 
 fix16_t cpVector3Length(const cpVector3 v) {
@@ -143,67 +146,35 @@ cpVector3 cpVector3Normalize(const cpVector3 v) {
 /* cpVector3i */
 
 cpVector3i cpVector3iAdd(const cpVector3i a, const cpVector3i b) {
-    return (cpVector3i) {
-        a.x + b.x,
-        a.y + b.y,
-        a.z + b.z,
-    };
+    I3_OP(+)
 }
 
 cpVector3i cpVector3iAddValue(const cpVector3i a, const int b) {
-    return (cpVector3i) {
-        a.x + b,
-        a.y + b,
-        a.z + b,
-    };
+    I3_OP_VAL(+)
 }
 
 cpVector3i cpVector3iSubtract(const cpVector3i a, const cpVector3i b) {
-    return (cpVector3i) {
-        a.x - b.x,
-        a.y - b.y,
-        a.z - b.z,
-    };
+    I3_OP(-)
 }
 
 cpVector3i cpVector3iSubtractValue(const cpVector3i a, const int b) {
-    return (cpVector3i) {
-        a.x - b,
-        a.y - b,
-        a.z - b,
-    };
+    I3_OP_VAL(-)
 }
 
 cpVector3i cpVector3iMultiply(const cpVector3i a, const cpVector3i b) {
-    return (cpVector3i) {
-        a.x * b.x,
-        a.y * b.y,
-        a.z * b.z,
-    };
+    I3_OP(*)
 }
 
 cpVector3i cpVector3iMultiplyValue(const cpVector3i a, const int b) {
-    return (cpVector3i) {
-        a.x * b,
-        a.y * b,
-        a.z * b,
-    };
+    I3_OP_VAL(*)
 }
 
 cpVector3i cpVector3iDivide(const cpVector3i a, const cpVector3i b) {
-    return (cpVector3i) {
-        a.x / b.x,
-        a.y / b.y,
-        a.z / b.z,
-    };
+    I3_OP(/)
 }
 
 cpVector3i cpVector3iDivideValue(const cpVector3i a, const int b) {
-    return (cpVector3i) {
-        a.x / b,
-        a.y / b,
-        a.z / b,
-    };
+    I3_OP_VAL(/)
 }
 
 cpVector3i cpVector3iNegate(const cpVector3i v) {
--- cplib/vector.h
+++ cplib/vector.h
@@ -9,6 +9,14 @@
 
 
 typedef struct {
+    fix16_t x, y;
+} cpVector2;
+
+typedef struct {
+    int x, y;
+} cpVector2i;
+
+typedef struct {
     fix16_t x, y, z;
 } cpVector3;
 
@@ -16,6 +24,35 @@ typedef struct {
     int x, y, z;
 } cpVector3i;
 
+
+// I was bored.
+
+cpVector2 cpVector2Add(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2AddValue(cpVector2 a, fix16_t b); // uses software floats!
+cpVector2 cpVector2Subtract(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2SubtractValue(cpVector2 a, fix16_t b); // uses software floats!
+cpVector2 cpVector2Multiply(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2MultiplyValue(cpVector2 a, fix16_t b); // uses software floats!
+cpVector2 cpVector2Divide(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2DivideValue(cpVector2 a, fix16_t b); // uses software floats!
+
+cpVector2 cpVector2Negate(cpVector2 v); // uses software floats!
+fix16_t cpVector2Length(cpVector2 v); // uses software floats!
+fix16_t cpVector2Distance(cpVector2 a, cpVector2 b); // uses software floats!
+fix16_t cpVector2DotProduct(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2CrossProduct(cpVector2 a, cpVector2 b); // uses software floats!
+cpVector2 cpVector2Normalize(cpVector2 v); // uses software floats!
+
+cpVector2i cpVector2iAdd(cpVector2i a, cpVector2i b);
+cpVector2i cpVector2iAddValue(cpVector2i a, int b);
+cpVector2i cpVector2iSubtract(cpVector2i a, cpVector2i b);
+cpVector2i cpVector2iSubtractValue(cpVector2i a, int b);
+cpVector2i cpVector2iMultiply(cpVector2i a, cpVector2i b);
+cpVector2i cpVector2iMultiplyValue(cpVector2i a, int b);
+cpVector2i cpVector2iDivide(cpVector2i a, cpVector2i b);
+cpVector2i cpVector2iDivideValue(cpVector2i a, int b);
+
+cpVector2i cpVector2iNegate(cpVector2i v);
 
 cpVector3 cpVector3Add(cpVector3 a, cpVector3 b); // uses software floats!
 cpVector3 cpVector3AddValue(cpVector3 a, fix16_t b); // uses software floats!
