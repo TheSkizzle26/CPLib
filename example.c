f56ea994172fbdbbfa057bb745eeefef43fd8ad4
TheSkizzle26 <timobarth12@outlook.de>
2026-04-16 17:26:58 +0200

    removed commented code line

--- example.c
+++ example.c
@@ -37,7 +37,6 @@ void init() {
         CP_PIXEL_FORMAT_RGB565,
         malloc(sizeof(data))
     };
-    // memset(texture.data, 0, sizeof(data));
     memcpy(texture.data, data, sizeof(data));
 
     texX = 100;
